7ed4b38b4b435cc273ad6515397c89651b4585d7
Noelle Daley <adriannenoelle@gmail.com>
2015-08-31 20:06:56 -0700

    added additional questions

--- linked_lists.py
+++ linked_lists.py
@@ -17,3 +17,19 @@ def remove_duplicates(ll):
             current.next = current.next.next
 
         current = current.next
+
+"""
+Determine whether or not a linked list has a loop in it.
+"""
+
+"""
+Given a list, check if the sum of a list is 0 without using the built in sum method.
+
+Try using a second pointer(index) to make more efficient.
+"""
+
+"""
+Count number of zero pairs in a list.
+
+(Try using a dictionary to make more efficient)
+"""
