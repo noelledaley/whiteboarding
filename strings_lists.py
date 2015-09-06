9e675539a75027091f363f60a6aa86ee9f4a321d
Noelle Daley <adriannenoelle@gmail.com>
2015-09-06 10:46:50 -0700

    setting up functions

---
+++ strings_lists.py
@@ -0,0 +1,18 @@
+
+def check_zero_sum(lst):
+    """
+    Given a list, check if the sum of a list is 0 without using the built in sum method.
+    """
+
+
+def check_zero_sum2(lst):
+    """Same as above, but try using a second pointer(index) to make more efficient."""
+
+
+def count_zero_pairs(lst):
+
+    """Count number of zero pairs in a list."""
+
+
+def count_zero_pairs2(lst):
+    """Same as above, but try using a dictionary to make more efficient."""
