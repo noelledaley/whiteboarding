73d953e79b1afb90b041d21df2f78abdefa91622
Noelle Daley <adriannenoelle@gmail.com>
2015-09-05 17:58:36 -0700

    added docstrings

--- data_structures.py
+++ data_structures.py
@@ -1,10 +1,12 @@
 class LinkedList(object):
+    """A sequence of Nodes."""
 
     def __init__(self):
         self.head = None
 
 
 class Node(object):
+    """A basic unit of a LinkedList or Tree."""
 
     def __init__(self, data):
         self.data = data
